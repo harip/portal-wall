8d35764e07761aeb34730d2856f6e59ce48328e7
Haricharan Pulugurta <charanp@gmail.com>
2026-01-10 22:16:49 -0500

    Auto-deploy: 2026-01-10 22:16:49

--- next.config.js
+++ next.config.js
@@ -3,9 +3,11 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   reactStrictMode: true,
-  output:'export',
-  basePath : isProd ? '/portal-wall' : '',
-  assetPrefix: isProd ? '/portal-wall' : '',
-}
+  output: 'export',
+  basePath: isProd ? '/portal-wall' : '',
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
